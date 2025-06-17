e48109d1b9ac56bbb98efabf30ce1e271cf44b6d
jiyoung61 <resiyo@naver.com>
2025-06-17 15:29:53 +0900

    inherit `ABC` class for `AbstractSSD`

--- ssd/abstract_ssd.py
+++ ssd/abstract_ssd.py
@@ -1,6 +1,10 @@
-class AbstractSSD:
+from abc import ABC, abstractmethod
+
+class AbstractSSD(ABC):
+    @abstractmethod
     def read(self, address: int) -> None:
         pass
 
+    @abstractmethod
     def write(self, address: int, data: str) -> None:
         pass
