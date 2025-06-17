2c58fa1a1215087f389a732499aa9944f7ce1b55
inpyo <winsowss@hanmail.net>
2025-06-18 08:54:54 +0900

    [Feature] 검증기 읽기 성공

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -2,6 +2,7 @@ import pytest
 from validator import Validator
 
 CORRECT_WRITE_SENTENCE="w 0 0xFFFFFFFF"
+CORRECT_READ_SENTENCE="r 0"
 
 def test_검증기_실행_테스트():
     validator = Validator()
@@ -14,6 +15,10 @@ def test_검증기_쓰기_올바른_입력_성공():
 @pytest.mark.parametrize("wrong_input", ["w", "0", "0xFFFFFFFF",
                                          "w 0", "w 0xFFFFFFFF", "0 0xFFFFFFFF",
                                          "w 0 0xFFFFFFFF 0xFFFFFFFF"])
-def test_검증기_쓰기_틀린_입력_잘못된_입력_길이(wrong_input):
+def test_검증기_쓰기_잘못된_입력_길이(wrong_input):
     validator = Validator()
     assert validator.run(wrong_input) == False
+    
+def test_검증기_읽기_올바른_입력_성공():
+    validator = Validator()
+    assert validator.run(CORRECT_READ_SENTENCE) == True
--- validator.py
+++ validator.py
@@ -1,8 +1,19 @@
 class Validator:
+    def valid_write_sentence(self, split_sentence:list):
+        if split_sentence[0] == 'w' and len(split_sentence) == 3:
+            return True
+
+    def valid_read_sentence(self, split_sentence:list):
+        if split_sentence[0] == 'r' and len(split_sentence) == 2:
+            return True
+
     def run(self, sentence:str):
         split_sentence = sentence.split(" ")
 
-        if(len(split_sentence) == 3):
+        if self.valid_write_sentence(split_sentence):
+            return True
+
+        if self.valid_read_sentence(split_sentence):
             return True
 
         return False
