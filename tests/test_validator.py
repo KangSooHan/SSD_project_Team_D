311cc018dfd7219d70274d317f7d3d42c2cffd9d
inpyo <winsowss@hanmail.net>
2025-06-18 08:49:42 +0900

    [Feature] 검증기 올바른 쓰기 성공

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,6 +1,13 @@
 import pytest
 from validator import Validator
 
+CORRECT_WRITE_SENTENCE="w 0 0xFFFFFFFF"
+
 def test_검증기_실행_테스트():
     validator = Validator()
     assert validator.run("test_input")
+
+def test_검증기_쓰기_올바른_입력_성공():
+    validator = Validator()
+    assert validator.run(CORRECT_WRITE_SENTENCE) == True
+
