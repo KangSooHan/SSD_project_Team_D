b3452f237ff562d78bb434586e63369f820d2a46
jiyoung61 <resiyo@naver.com>
2025-06-17 16:46:57 +0900

    [refactor] ssd mock fixture 도입

--- tests/test_testscenario.py
+++ tests/test_testscenario.py
@@ -5,13 +5,14 @@ from shell.commands.testscenario1 import TestScenario1
 from ssd import ssd
 from ssd.abstract_ssd import AbstractSSD
 
+@pytest.fixture
+def ssd_mock(mocker: MockerFixture):
+    return mocker.Mock(spe=AbstractSSD)
 
-def test_테스트시나리오1_객체를_생성한다(mocker: MockerFixture):
-    sdd_mock = mocker.Mock(spec=AbstractSSD)
-    sut = TestScenario1(sdd_mock)
+def test_테스트시나리오1_객체를_생성한다(ssd_mock):
+    sut = TestScenario1(ssd_mock)
     assert True
 
-def test_테스트시나리오1_객체는_SSD_인터페이스를_의존성으로_주입받는다(mocker: MockerFixture):
-    sdd_mock = mocker.Mock(spec=AbstractSSD)
-    sut = TestScenario1(sdd_mock)
+def test_테스트시나리오1_객체는_SSD_인터페이스를_의존성으로_주입받는다(ssd_mock):
+    sut = TestScenario1(ssd_mock)
     assert True
