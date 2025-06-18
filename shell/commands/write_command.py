8b38d8ff47a8a2f59d33fc86a6edc5b8e19b3edf
chiwon.park <chiwona123@gmail.com>
2025-06-18 09:06:46 +0900

    [green] shell Write Command 초기 commint ( branch가 꼬여서 기존 HelpCpmmand는 삭제하고 별도 branch 생성 예정 )

--- shell/commands/write_command.py
+++ shell/commands/write_command.py
@@ -1,10 +1,9 @@
 from shell.commands.base import Command
 from ssd.abstract_ssd import AbstractSSD
 
-class HelpCommand:
+class WriteCommand:
     def __init__(self, ssd: AbstractSSD):
         self._ssd = ssd
 
-    def execute(self) -> None:
-        self._ssd.write("")
-        return True
+    def execute(self, lba, value) -> None:
+        self._ssd.write(lba, value)
---
+++ tests/test_shell_write_command.py
@@ -0,0 +1,14 @@
+import pytest
+from ssd.abstract_ssd import AbstractSSD
+from shell.commands.write_command import WriteCommand
+
+def test_help명령어(mocker):
+    # arrange
+    ssd: AbstractSSD = mocker.Mock()
+    write_cmd = WriteCommand(ssd)
+
+    # act
+    write_cmd.execute(3, 0xAAAABBBB)
+
+    # assert
+    assert ssd.write.call_count == 1
