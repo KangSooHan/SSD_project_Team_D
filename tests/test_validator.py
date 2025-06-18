d688c6da0cde9523593852cf4df7630305998b12
inpyo <winsowss@hanmail.net>
2025-06-18 09:00:32 +0900

    [Feature] 검증기 앞뒤 공백 문자

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,26 +4,41 @@ from validator import Validator
 CORRECT_WRITE_SENTENCE="w 0 0xFFFFFFFF"
 CORRECT_READ_SENTENCE="r 0"
 
-def test_검증기_실행_테스트():
-    validator = Validator()
+@pytest.fixture
+def validator():
+    return Validator()
+
+def test_검증기_실행_테스트(validator):
     assert validator.run("test_input")
 
-def test_검증기_쓰기_올바른_입력_성공():
-    validator = Validator()
+def test_검증기_쓰기_올바른_입력_성공(validator):
     assert validator.run(CORRECT_WRITE_SENTENCE) == True
 
 @pytest.mark.parametrize("wrong_input", ["w", "0", "0xFFFFFFFF",
                                          "w 0", "w 0xFFFFFFFF", "0 0xFFFFFFFF",
                                          "w 0 0xFFFFFFFF 0xFFFFFFFF"])
-def test_검증기_쓰기_잘못된_입력_길이(wrong_input):
-    validator = Validator()
+def test_검증기_쓰기_잘못된_입력_길이(wrong_input, validator):
     assert validator.run(wrong_input) == False
     
-def test_검증기_읽기_올바른_입력_성공():
-    validator = Validator()
+def test_검증기_읽기_올바른_입력_성공(validator):
     assert validator.run(CORRECT_READ_SENTENCE) == True
 
 @pytest.mark.parametrize("wrong_input", ["r", "0", "r 0 0"])
-def test_검증기_읽기_잘못된_입력_길이(wrong_input):
-    validator = Validator()
+def test_검증기_읽기_잘못된_입력_길이(wrong_input, validator):
     assert validator.run(wrong_input) == False
+
+def make_wrong_sentences():
+    wrong_sentences = []
+    wrong_sentences.append(" " + CORRECT_READ_SENTENCE)
+    wrong_sentences.append(CORRECT_READ_SENTENCE + " ")
+    wrong_sentences.append(" " + CORRECT_READ_SENTENCE + " ")
+    wrong_sentences.append(" " + CORRECT_WRITE_SENTENCE)
+    wrong_sentences.append(CORRECT_WRITE_SENTENCE + " ")
+    wrong_sentences.append(" " + CORRECT_WRITE_SENTENCE + " ")
+
+    return wrong_sentences
+
+def test_검증기_앞뒤_공백_문자(validator):
+    wrong_sentences = make_wrong_sentences()
+    for wrong_sentence in wrong_sentences:
+        assert validator.run(wrong_sentence) == True
--- validator.py
+++ validator.py
@@ -7,7 +7,12 @@ class Validator:
         if split_sentence[0] == 'r' and len(split_sentence) == 2:
             return True
 
+    def preprocess_sentence(self, sentence:str):
+        sentence = sentence.strip()
+        return sentence
+
     def run(self, sentence:str):
+        sentence = self.preprocess_sentence(sentence)
         split_sentence = sentence.split(" ")
 
         if self.valid_write_sentence(split_sentence):
