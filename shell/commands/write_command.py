b26641ac8db4517cdbc62e597e297bec20f03598
chiwon.park <chiwona123@gmail.com>
2025-06-18 09:31:51 +0900

    [refactor] write시 VALUE 정상 검증

--- shell/commands/write_command.py
+++ shell/commands/write_command.py
@@ -1,11 +1,17 @@
 from shell.commands.base import Command
 from ssd.abstract_ssd import AbstractSSD
+import re
 
 class WriteCommand:
     def __init__(self, ssd: AbstractSSD):
         self._ssd = ssd
 
-    def execute(self, lba, value) -> None:
+    def execute(self, lba:int, value:str) -> None:
         if lba < 0 or lba > 99 :
             raise Exception
+        if not self._is_valid_hex_regex(value):
+            raise Exception
         self._ssd.write(lba, value)
+
+    def _is_valid_hex_regex(self, value: str) -> bool:
+        return bool(re.fullmatch(r'0x[0-9A-Fa-f]{8}', value))
--- tests/test_shell_write_command.py
+++ tests/test_shell_write_command.py
@@ -8,7 +8,7 @@ def test_write_성공(mocker):
     write_cmd = WriteCommand(ssd)
 
     # act
-    write_cmd.execute(3, 0xAAAABBBB)
+    write_cmd.execute(3, '0xAAAABBBB')
 
     # assert
     assert ssd.write.call_count == 1
@@ -20,5 +20,34 @@ def test_write_실패_LBA범위초과(mocker):
 
     # act & assert
     with pytest.raises(Exception):
-        write_cmd.execute(100, 0xAAAABBBB)
+        write_cmd.execute(100, '0xAAAABBBB')
+
+
+def test_write_실패_VALUE길이_10자초과(mocker):
+    # arrange
+    ssd: AbstractSSD = mocker.Mock()
+    write_cmd = WriteCommand(ssd)
+
+    # act & assert
+    with pytest.raises(Exception):
+        write_cmd.execute(3, '0xAAAABBBBB')
+
+
+def test_write_실패_VALUE길이_10자미만(mocker):
+    # arrange
+    ssd: AbstractSSD = mocker.Mock()
+    write_cmd = WriteCommand(ssd)
+
+    # act & assert
+    with pytest.raises(Exception):
+        write_cmd.execute(3, '0xAAAABBB')
+
+def test_write_실패_VALUE값_16진수범위초과(mocker):
+    # arrange
+    ssd: AbstractSSD = mocker.Mock()
+    write_cmd = WriteCommand(ssd)
+
+    # act & assert
+    with pytest.raises(Exception):
+        write_cmd.execute(3, '0xAAAAFFFG')
 
