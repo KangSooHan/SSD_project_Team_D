442e929d0219ba8acac8990dea835b08d6f0921c
chiwon.park <chiwona123@gmail.com>
2025-06-18 13:08:43 +0900

    [refactor] help msg textwrap.dedent 사용하여 포매팅

--- shell/commands/help_command.py
+++ shell/commands/help_command.py
@@ -1,16 +1,16 @@
-class HelpCommand:
-    def __init__(self):
-        pass
+import textwrap
 
+
+class HelpCommand:
     def execute(self) -> None:
         print("팀명: Discovery | 팀원: 강수한, 이후광, 윤창흠, 김지영, 이지훈, 박치원")
         print("명령어 사용 방법 : ")
-        command_usage = '''
+        command_usage = textwrap.dedent('''
         - write : write {LBA} {VALUE}
         - read : read {LBA}
         - exit : exit
         - help : help
         - fullwrite : fullwrite {VALUE}
         - fullread : fullread
-        '''
+        ''').strip()
         print(command_usage)
