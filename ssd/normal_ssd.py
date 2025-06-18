50083a5d1057464d65bfe404e6a122692e7b9f66
hyowon.moon <hyowon.moon@samsung.com>
2025-06-18 12:40:19 +0900

    [refactor] 1. file write 관련 함수 추가 2. _write_output 함수 삭제 3. overwrite 관련 멤버 변수 -> 지역변수로 수정

--- ssd/normal_ssd.py
+++ ssd/normal_ssd.py
@@ -3,7 +3,6 @@ import os
 from ssd.abstract_ssd import AbstractSSD
 
 
-
 class NormalSSD(AbstractSSD):
     DEFAULT_NAND_FILE: str = "ssd_nand.txt"
     DEFAULT_OUTPUT_FILE: str = "ssd_output.txt"
@@ -21,19 +20,16 @@ class NormalSSD(AbstractSSD):
         self._output_file: str = output_file
         self._valid_lba_range: range = lba_range
 
-        self._overwrite_flag = False
-
         if not os.path.exists(self._nand_file):
             open(self._nand_file, "w").close()
 
     def read(self, address: int) -> None:
         if address not in self._valid_lba_range:
-            self._write_output(self.INVALID_OUTPUT)
+            self._file_write(self._output_file,"w",self.INVALID_OUTPUT)
             return
-          
-        value = self._load_value_from_nand(address)
-        self._write_output(value)
 
+        value = self._load_value_from_nand(address)
+        self._file_write(self._output_file, "w", value)
     def _load_value_from_nand(self, address: int) -> str:
         with open(self._nand_file, "r") as f:
             for line in f:
@@ -43,32 +39,34 @@ class NormalSSD(AbstractSSD):
                     return value
         return self.EMPTY_VALUE
 
-
-    def _write_output(self, value: str) -> None:
-        with open(self._output_file, "w") as f:
+    def _file_write(self, file_name : str, mode : str, value : str) -> None:
+        with open(file_name, mode) as f:
             f.write(value)
 
     def write(self, address: int, data: int) -> None:
+        overwrite_flag = False
         if address not in self._valid_lba_range:
-            self._write_output(self.INVALID_OUTPUT)
+            self._file_write(self._output_file,"w",self.INVALID_OUTPUT)
             return
 
         with open(self.DEFAULT_NAND_FILE, 'r+') as file:
             lines = file.readlines()  # 전체 줄을 미리 읽고
-            if not lines:
-                file.write(f'{address} 0x{data:08X}\n')
-                return
+        if not lines:
+            self._file_write(self.DEFAULT_NAND_FILE,'r+',f'{address} 0x{data:08X}\n')
+            return
+        with open(self.DEFAULT_NAND_FILE, 'r+') as file:
             file.seek(0)  # 파일 처음으로 이동
             file.truncate()  # 기존 파일 내용을 비움
 
             for line in lines:
                 written_lba, written_data = line.strip().split()
                 new_line = line
-                if int(written_lba) == address and not self._overwrite_flag:
-                    self._overwrite_flag = True
+                if int(written_lba) == address and not overwrite_flag:
+                    overwrite_flag = True
                     new_data = f'0x{data:08X}'  # 16진수로 포맷
                     new_line = f'{written_lba} {new_data}\n'
                 file.write(new_line)
-            if not self._overwrite_flag:
+
+            if not overwrite_flag:
                 file.write(f'{address} 0x{data:08X}\n')
 
