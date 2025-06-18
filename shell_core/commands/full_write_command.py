b0b60979d49a1228ac7f93306ef28a26c25ec7ae
chiwon.park <chiwona123@gmail.com>
2025-06-18 17:31:17 +0900

    [bugfix] fullwirte 수정

--- shell_core/commands/full_write_command.py
+++ shell_core/commands/full_write_command.py
@@ -2,10 +2,11 @@ from ssd_core.abstract_ssd import AbstractSSD
 
 
 class FullWriteCommand:
-    def __init__(self, ssd: AbstractSSD):
+    def __init__(self, ssd: AbstractSSD, value: str):
         self._ssd = ssd
         self._loop_count = 100
+        self._value = value
 
-    def execute(self, value: int) -> None:
+    def execute(self) -> None:
         for lba in range(self._loop_count):
-            self._ssd.write(lba, value)
+            self._ssd.write(lba, self._value)
