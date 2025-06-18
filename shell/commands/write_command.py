5366e6cc97b88b8cd193bcb22f32fa34e6f36101
chiwon.park <chiwona123@gmail.com>
2025-06-18 10:13:47 +0900

    [refactor] write시 VALUE 검증은 Validator 로 이관

--- shell/commands/write_command.py
+++ shell/commands/write_command.py
@@ -6,12 +6,7 @@ class WriteCommand:
     def __init__(self, ssd: AbstractSSD):
         self._ssd = ssd
 
-    def execute(self, lba:int, value:str) -> None:
+    def execute(self, lba:int, value:int) -> None:
         if lba < 0 or lba > 99 :
             raise Exception
-        if not self._is_valid_hex_regex(value):
-            raise Exception
         self._ssd.write(lba, value)
-
-    def _is_valid_hex_regex(self, value: str) -> bool:
-        return bool(re.fullmatch(r'0x[0-9A-Fa-f]{8}', value))
