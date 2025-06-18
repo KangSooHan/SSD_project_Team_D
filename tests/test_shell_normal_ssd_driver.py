9d64f2fe9328ea1b8b2da1e5510258c072fc9f8d
chiwon.park <chiwona123@gmail.com>
2025-06-18 14:32:27 +0900

    [refactor] normal ssd driver read 기능 검증

--- tests/test_shell_normal_ssd_driver.py
+++ tests/test_shell_normal_ssd_driver.py
@@ -2,6 +2,7 @@ import os
 import pytest
 from shell.normal_ssd_driver import NormalSSDDriver
 from shell.commands.write_command import WriteCommand
+from shell.commands.read_command import ReadCommand
 
 @pytest.mark.parametrize("write_lba, data", [
     (0, "0x11111111"),
@@ -12,5 +13,14 @@ def test_normal_ssd_driver_write_성공(mocker, write_lba, data):
     write_cmd = WriteCommand(ssd)
 
     write_cmd.execute(write_lba, data)
-
     assert ssd.write.call_count == 1
+
+@pytest.mark.parametrize("read_lba", [0,99])
+def test_normal_ssd_driver_read_성공(mocker, read_lba):
+    # arrange
+    ssd: NormalSSDDriver = mocker.Mock()
+    read_cmd = ReadCommand(ssd)
+
+    rtn = read_cmd.execute(read_lba)
+    assert ssd.read.call_count == 1
+
