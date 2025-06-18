3793fdfffe2e07c6d053e58e673aa8631d64ffa4
chiwon.park <chiwona123@gmail.com>
2025-06-18 12:35:39 +0900

    [green] exit command 초기 commit

---
+++ tests/test_shell_exit_command.py
@@ -0,0 +1,10 @@
+import pytest
+from shell.commands.exit_command import ExitCommand
+
+
+def test_write_성공(mocker):
+    # arrange
+    exit_cmd = ExitCommand()
+
+    # act & assert
+    assert True
