8f3e1d48ce2d44b9b8119055029e072649916511
chiwon.park <chiwona123@gmail.com>
2025-06-18 11:04:53 +0900

    [refactor] read 객체 호출 실패 케이스 검증

--- shell/commands/read_command.py
+++ shell/commands/read_command.py
@@ -4,3 +4,7 @@ class ReadCommand:
     def __init__(self, ssd: AbstractSSD):
         self._ssd = ssd
 
+    def execute(self, lba:int) -> None:
+        if lba < 0 or lba > 99:
+            raise Exception
+        self._ssd.read(lba)
--- tests/test_shell_read.py
+++ tests/test_shell_read.py
@@ -2,9 +2,25 @@ import pytest
 from ssd.abstract_ssd import AbstractSSD
 from shell.commands.read_command import ReadCommand
 
-def test_read_성공(mocker):
+@pytest.mark.parametrize("lba",[1,2,3,4,99])
+def test_read_성공(mocker, lba):
     # arrange
     ssd: AbstractSSD = mocker.Mock()
     read_cmd = ReadCommand(ssd)
 
-    assert True
+    # act
+    read_cmd.execute(lba)
+
+    # assert
+    assert ssd.read.call_count == 1
+
+@pytest.mark.parametrize("lba",[-1,100])
+def test_read_실패(mocker, lba):
+    # arrange
+    ssd: AbstractSSD = mocker.Mock()
+    read_cmd = ReadCommand(ssd)
+
+    # act & assert
+    with pytest.raises(Exception):
+        read_cmd.execute(lba)
+
