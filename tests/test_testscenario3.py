6e7cfab0f81246c926ef1969c7d3bf8c3e70bb77
jiyoung61 <resiyo@naver.com>
2025-06-18 13:20:31 +0900

    [feat] TestScenario3 전체 시나리오 실패, 성공 테스트 추가

---
+++ tests/test_testscenario3.py
@@ -0,0 +1,30 @@
+import random
+from unittest.mock import call
+
+import pytest
+from pytest_mock import MockerFixture
+
+from shell.commands.testscenario3 import TestScenario3
+from shell.commands.write_command import WriteCommand
+from ssd import ssd
+from ssd.abstract_ssd import AbstractSSD
+
+
+@pytest.fixture
+def ssd_mock(mocker: MockerFixture):
+    return mocker.Mock(spec=AbstractSSD)
+
+
+def test_전체시나리오에_실패할경우_FAIL값을_리턴한다(ssd_mock):
+    sut = TestScenario3(ssd_mock)
+    assert sut.execute() == "FAIL"
+
+
+def test_전체시나리오에_성공할경우_PASS값을_리턴한다(ssd_mock):
+    sut = TestScenario3(ssd_mock)
+    random.seed(0)
+    ssd_mock.read.side_effect = [f'0x{random.randint(1, 10):08X}' for i in range(200 * 2)]
+    
+    # sut.execute() 실행 시 동일한 시퀀스의 랜덤값을 추출하기 위해 시드 초기화
+    random.seed(0)
+    assert sut.execute() == "PASS"
