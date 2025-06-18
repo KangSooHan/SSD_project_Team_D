28cf1786fdc8fda02c89b3ed81230b8f750ceac2
hyowon.moon <hyowon.moon@samsung.com>
2025-06-18 16:34:45 +0900

    [feat] shell read Test Case 추가

--- tests/test_shell_read.py
+++ tests/test_shell_read.py
@@ -1,5 +1,6 @@
 import pytest
 from ssd_core.abstract_ssd import AbstractSSD
+from ssd_core.normal_ssd import NormalSSD
 from shell_core.commands.read_command import ReadCommand
 
 
@@ -25,3 +26,31 @@ def test_read_실패(mocker, lba):
     # act & assert
     with pytest.raises(Exception):
         read_cmd.execute(lba)
+
+
+@pytest.mark.parametrize("lba", [1, 2, 3, 4, 99])
+def test_read_올바른값_성공(mocker, lba):
+    # arrange
+    ssd: NormalSSD = mocker.Mock()
+    ssd.read.return_value = 0xABABABAB
+
+    read_cmd = ReadCommand(ssd)
+
+    # act
+    result = read_cmd.execute(lba)
+
+    # assert
+    assert result == 0xABABABAB
+
+
+@pytest.mark.parametrize("lba", [-1, 100, 2000])
+def test_read_잘못된범위_실패(mocker, lba):
+    # arrange
+    ssd: NormalSSD = mocker.Mock()
+
+    read_cmd = ReadCommand(ssd)
+
+    # act & assert
+    with pytest.raises(Exception):
+        read_cmd.execute(lba)
+
