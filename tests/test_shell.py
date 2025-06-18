98c5f07566be7ee6b50557b68f3163b3bef48154
chhm.yoon <chhm.yoon@samsung.com>
2025-06-18 16:28:48 +0900

    [feat] shell test 의 shell_system_call 함수명 변경, 여러개의_명령어_실행테스트 삭제

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -15,7 +15,7 @@ def init_nand_file_for_test():
             f.write(content + '\n')
 
 
-def pipe(input_script):
+def shell_system_call(input_script):
     """간단한 subprocess 방식 pipe 함수"""
     process = subprocess.Popen(
         ["python", "../shell.py"],
@@ -44,59 +44,31 @@ def pipe(input_script):
         return ""
 
 
-def test_shell_실행테스트():
-    process = subprocess.Popen(
-        ["python", "shell.py"],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        encoding='utf-8',
-        errors='ignore'
-    )
-    try:
-        stdout, stderr = process.communicate(input="exit", timeout=3)
-        if stdout is None:
-            stdout = ""
-        assert "Test Shell Application" in stdout
-        assert "Exit" in stdout
-    except subprocess.TimeoutExpired:
-        process.kill()
-        stdout, stderr = process.communicate()
-        pytest.fail("Mock shell 타임 아웃 에러")
-        return None
-    except Exception as e:
-        process.kill()
-        process.wait()
-        pytest.fail(f"Subprocess 에러: {e}")
-        return None
+def test_shell_단순실행테스트():
+    input_script = ""
+    stdout = shell_system_call(input_script)
 
-
-def test_shell_exit_실행테스트():
-    input_script = "exit"
-    stdout = pipe(input_script)
-    assert "Exit" in stdout
-
+    assert "Test Shell Application" in stdout
 
 def test_shell_read_실행테스트():
     init_nand_file_for_test()
 
     input_script = "read 10\nexit"
-    stdout = pipe(input_script)
+    stdout = shell_system_call(input_script)
     read_message = "[Read] LBA 00 : 0x00000005"
     assert read_message in stdout
 
 
 def test_shell_write_실행테스트():
     input_script = "write 0 0x00000001\nexit"
-    stdout = pipe(input_script)
+    stdout = shell_system_call(input_script)
     write_message = "[Write] Done"
     assert write_message in stdout
 
 
 def test_shell_help_실행테스트():
     input_script = "help"
-    stdout = pipe(input_script)
+    stdout = shell_system_call(input_script)
     print(stdout)
     assert "팀명: Discovery | 팀원: 강수한, 이후광, 윤창흠, 김지영, 이지훈, 박치원" in stdout
     assert "명령어 사용 방법 :" in stdout
@@ -107,21 +79,10 @@ def test_shell_help_실행테스트():
     assert "fullwrite : fullwrite {VALUE}" in stdout
     assert "fullread : fullread" in stdout
 
-
-def test_shell_여러개의_명령어_실행테스트():
-    """여러 명령어 테스트"""
-    input_script = "help\nread 0\nwrite 0 0x00000001\nexit"
-    stdout = pipe(input_script)
-    assert "팀명: Discovery | 팀원: 강수한, 이후광, 윤창흠, 김지영, 이지훈, 박치원" in stdout
-    assert "[Read] LBA 0 :" in stdout
-    assert "[Write] Done" in stdout
-    assert "Exit" in stdout
-
-
 def test_shell_fullwrite_실행테스트():
     """fullwrite 테스트"""
     input_script = "fullwrite 0xABCDFFFF\nexit"
-    stdout = pipe(input_script)
+    stdout = shell_system_call(input_script)
     assert 1 == 1
 
 def test_shell_fullread_실행테스트():
@@ -129,7 +90,7 @@ def test_shell_fullread_실행테스트():
     init_nand_file_for_test()
 
     input_script = "fullread\nexit"
-    stdout = pipe(input_script)
+    stdout = shell_system_call(input_script)
     expected = ""
     for _ in range(100):
         expected += "0x00000005\n"
