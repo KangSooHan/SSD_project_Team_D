cfc531ed05c2f32f3f51915c83f632be7554fdad
jiyoung61 <resiyo@naver.com>
2025-06-19 17:18:32 +0900

    [feat] erase, write 위치를 찾는 함수 구현

--- ssd_core/discovery_buffer_optimizer.py
+++ ssd_core/discovery_buffer_optimizer.py
@@ -10,9 +10,16 @@ VALUE_MARK = 2
 class DiscoveryBufferOptimizer(AbstractBufferOptimizer):
     def calculate(self, buffer_lst: list[Packet]) -> list[Packet]:
         """
-        
+        plan
+        - erase 영역을 모두 모은다
+        - write 영역을 모두 모은다. erase가 덮어쓰는 경우는 제외 한다.
+        - erase 이후에 write 되는 erase 영역을 mark 영역으로 정의하고 마킹한다.
+        - mark 영역에 erase를 수행할지 말지를 0/1로 구분하여 경우의 수를 생성한다.
+        - 경우의 수는 erase 1번 write 4번인 경우 2^4 = 16개가 생성된다.
+        - 16개의 erase 경우에 수에 대해, 최소로 erase 하는 카운트 + write 카운트가 최적해가 된다. (라고 믿고 싶다)
+        - erase를 최소로 하는 카운트는 greedy로 순차 선택하여도 최적해가 나온다. (고 밑고 있다)
+        - 순회 하는 중,  current_count와 동률이 나올 경우 skip 후 다음 해를 시도 한다.
         """
-
         # project values
         erase_lst = self.project_erase(buffer_lst)
         write_lst = self.project_write(buffer_lst)
