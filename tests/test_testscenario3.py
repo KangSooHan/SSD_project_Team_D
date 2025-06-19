d59ada38841f7adcc2833d9d10ac9bfa325e421c
inpyo <winsowss@hanmail.net>
2025-06-19 16:15:58 +0900

    [fix] test_scenario_read 부분에 fast_read로 적혀있던 버그 수정

--- tests/test_testscenario3.py
+++ tests/test_testscenario3.py
@@ -12,7 +12,7 @@ from ssd_core.abstract_ssd import AbstractSSD
 @pytest.fixture
 def ssd_mock(mocker: MockerFixture):
     mock = mocker.Mock(spec=AbstractSSD)
-    mock.fast_read_from.return_value = "0x00000000"
+    mock.read.return_value = "0x00000000"
     return mock
 
 
@@ -31,7 +31,7 @@ def test_전체시나리오에_성공할경우_PASS값을_출력한다(ssd_mock)
     sys.stdout = output
 
     random.seed(0)
-    ssd_mock.fast_read_from.side_effect = [f'0x{random.randint(1, 10):08X}' for i in range(200 * 2)]
+    ssd_mock.read.side_effect = [f'0x{random.randint(1, 10):08X}' for i in range(200 * 2)]
 
     # sut.execute() 실행 시 동일한 시퀀스의 랜덤값을 추출하기 위해 시드 초기화
     random.seed(0)
