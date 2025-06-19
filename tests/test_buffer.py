4a75ba51f55cc85974a3cdd4f4534ff17aa2ac7c
inpyo <winsowss@hanmail.net>
2025-06-19 11:02:52 +0900

    [fix] Buffer 기본 구현 완성

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -1,6 +1,7 @@
 import pytest
 from buffer_core.buffer import Buffer
 from pytest_mock import MockFixture
+from unittest.mock import patch
 
 @pytest.fixture
 def buffer(mocker:MockFixture):
@@ -11,12 +12,12 @@ def test_Buffer_구현_성공():
     buffer_test = Buffer()
     assert True
 
-
 def test_Buffer_insert_5보다_큰_입력(buffer):
-    for i in range(10):
-        buffer.insert(str(i))
+    with patch.object(buffer, 'flush', wraps=buffer.flush) as mock_flush:
+        for i in range(10):
+            buffer.insert(str(i))
 
-    assert buffer.insert.call_count == 10
+        mock_flush.call_count == 1
 
 def test_Buffer_insert_5보다_작은_입력(buffer):
     for i in range(3):
@@ -24,7 +25,8 @@ def test_Buffer_insert_5보다_작은_입력(buffer):
 
     assert buffer.insert.call_count == 3
 
-def test_Buffer_flush_5보다_작은_입력(buffer):
+def test_Buffer_flush_5보다_작은_입력():
+    buffer = Buffer()
     inputs = [str(i) for i in range(1, 4)]
     for input in inputs:
         buffer.insert(input)
@@ -32,13 +34,11 @@ def test_Buffer_flush_5보다_작은_입력(buffer):
     assert buffer._print() == inputs
     assert buffer.flush() == inputs
 
-def test_Buffer_flush_5보다_큰_입력(buffer):
+def test_Buffer_flush_5보다_큰_입력():
+    buffer = Buffer()
     inputs = [str(i) for i in range(1, 10)]
     for input in inputs:
         buffer.insert(input)
-
-    # flush가 한 번 호출되었는지 확인
-    buffer.flush.assert_called_once()
 
     # 출력 결과 검증
     assert buffer._print() == [str(i) for i in range(6, 10)]
