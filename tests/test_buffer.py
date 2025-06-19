95634e98ceea00e21ecf39f91a1a08fcfca1fca6
jiyoung61 <resiyo@naver.com>
2025-06-19 13:30:56 +0900

    [refactor] buffer insert 함수의 시그니처 변경

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -5,8 +5,8 @@ class FakeBuffer:
     def __init__(self):
         self.memory = []
 
-    def insert(self, value: str):
-        self.memory.append(value)
+    def insert(self, cmd: str, op1: int, op2: int):
+        self.memory.append((cmd, op1, op2))
         if self.len() == 5:
             self.flush()
 
@@ -20,6 +20,9 @@ class FakeBuffer:
 
     def _print(self):
         return self.memory
+
+    def optimize(self):
+        pass
 
 
 @pytest.fixture
@@ -38,7 +41,7 @@ def test_Buffer객체_초기_길이값은_0이다(buffer):
 
 def test_Buffer객체는_W_명령을_개별_인자로_분리한다(buffer):
     # W [LBA] [VALUE]
-    buffer.insert("W 0 0x00000000")
+    buffer.insert("W", 0, 0)
 
     assert buffer.len == 1
     assert len(buffer.memory[0]) == 3  # (CMD, LBA, VALUE)
@@ -49,7 +52,7 @@ def test_Buffer객체는_W_명령을_개별_인자로_분리한다(buffer):
 
 def test_Buffer객체는_E_명령을_개별_인자로_분리한다(buffer):
     # E [LBA] [SIZE]
-    buffer.insert("E 0 10")
+    buffer.insert("E", 0, 10)
 
     assert buffer.len == 1
     assert len(buffer.memory[0]) == 3  # (CMD, LBA, SIZE)
@@ -60,35 +63,41 @@ def test_Buffer객체는_E_명령을_개별_인자로_분리한다(buffer):
 
 def test_Buffer객체는_R_명령을_저장하지_않는다(buffer):
     # R [LBA]
-    buffer.insert("R 0")
+    buffer.insert("R", 0, 0)
 
     assert buffer.len == 0
 
 
 def test_Buffer객체는_flush_명령으로_buffer를_비운다(buffer):
-    buffer.insert("W 0 0x00000000")
+    buffer.insert("W", 0, 0)
+
+    assert buffer.len() == 1
+
     buffer.flush()
 
     assert buffer.len == 0
 
 
 def test_Buffer객체는_최적화대상이_아닌_명령에_대해_5개_항목을_유지한다(buffer):
-    buffer.insert("W 0 0x00000000")
-    buffer.insert("W 1 0x00000000")
-    buffer.insert("W 2 0x00000000")
-    buffer.insert("W 3 0x00000000")
-    buffer.insert("W 4 0x00000000")
+    buffer.insert("W", 0, 0)
+    buffer.insert("W", 1, 1)
+    buffer.insert("W", 2, 2)
+    buffer.insert("W", 3, 3)
+    buffer.insert("W", 4, 4)
 
     assert buffer.len == 5
 
 def test_Buffer객체는_최적화대상이_아닌_명령이_5개를초과하면_flush를_수행한다(buffer):
-    buffer.insert("W 0 0x00000000")
-    buffer.insert("W 1 0x00000000")
-    buffer.insert("W 2 0x00000000")
-    buffer.insert("W 3 0x00000000")
-    buffer.insert("W 4 0x00000000")
+    buffer.insert("W", 0, 0)
+    buffer.insert("W", 1, 1)
+    buffer.insert("W", 2, 2)
+    buffer.insert("W", 3, 3)
+    buffer.insert("W", 4, 4)
+
+    assert buffer.len == 5
+
     # flush
-    buffer.insert("W 5 0x00000000")
+    buffer.insert("W", 5, 5)
 
     assert buffer.len == 1
 
@@ -98,7 +107,10 @@ test cases for buffer optimization
 """
 @pytest.mark.skip
 def test_ignore_cmd_동일한_W_명령은_압축한다(buffer):
-    buffer.insert("W 0 0x00000000")
-    buffer.insert("W 0 0x00000000")
+    buffer.insert("W", 0, 0)
+    buffer.insert("W", 0, 0)
+
+    buffer.optimize()
 
     assert buffer.len == 1
+    assert buffer.memory[0] == ("W", 0, 0)
