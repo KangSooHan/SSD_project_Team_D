ca1ed18664d30091af0059339de9a7cec738a98e
hyowon.moon <hyowon.moon@samsung.com>
2025-06-19 17:03:00 +0900

    [feat] 1. Runner 구현 2. command_factory에 runner 타입 추가 3. validtor 수정

---
+++ command_core/shell_commands/runner.py
@@ -0,0 +1,48 @@
+import os
+import inspect
+from command_core.base_command import BaseCommand
+from command_core.shell_commands import testscenario
+from ssd_core.abstract_ssd import AbstractSSD
+from command_core.shell_commands.testscenario import TestScenario
+
+scenario_map = {
+    "1_FullWriteAndReadCompare": testscenario.TestScenario1,
+    "2_PartialLBAWrite": testscenario.TestScenario2,
+    "3_WriteReadAging": testscenario.TestScenario3,
+    "4_EraseAndWriteAging": testscenario.TestScenario4,
+}
+
+class Runner(BaseCommand):
+    def __init__(self, ssd: AbstractSSD):
+        self.ssd = ssd
+
+    def execute(self):
+        script_path = os.path.join(os.path.dirname(__file__), "..", "..", "shell_scripts.txt")
+        script_path = os.path.abspath(script_path)
+
+        if not os.path.exists(script_path):
+            print(script_path)
+            print("Error: shell_scripts.txt not exists")
+            return
+
+        with open(script_path, "r") as f:
+            lines = [line.strip() for line in f if line.strip()]
+
+        for scenario_name in lines:
+            try:
+                # testscenario 모듈에서 클래스 가져오기
+                scenario_class = scenario_map.get(scenario_name)
+                if scenario_class is None:
+                    print(f"[Runner] '{scenario_name}' not found")
+                    return
+
+                if not inspect.isclass(scenario_class) or not issubclass(scenario_class, TestScenario):
+                    print(f"[Runner] '{scenario_name}'는 유효한 TestScenario가 아닙니다.")
+                    return
+
+                print(f"{scenario_name} ___ Run...")
+                scenario = scenario_class(self.ssd)
+                scenario.execute()
+
+            except Exception as e:
+                print(f"[Runner] '{scenario_name}' 실행 중 오류 발생: {e}")
