f4c3ba0093f8d39662e64de751afb67c7760a66c
jiyoung61 <resiyo@naver.com>
2025-06-22 22:30:40 +0900

    [feat] optimizer 구현

--- ssd_core/discovery_buffer_optimizer.py
+++ ssd_core/discovery_buffer_optimizer.py
@@ -1,54 +1,150 @@
 import copy
-from itertools import product
+from itertools import product, combinations
 from ssd_core.abstract_buffer_optimizer import AbstractBufferOptimizer
 from validator import Packet
 
 VALUE_EMPTY = 0
 VALUE_VALID = 1
-VALUE_MARK = 2
+
 
 class DiscoveryBufferOptimizer(AbstractBufferOptimizer):
     def calculate(self, buffer_lst: list[Packet]) -> list[Packet]:
-        """
-        plan
-        - erase 영역을 모두 모은다
-        - write 영역을 모두 모은다. erase가 덮어쓰는 경우는 제외 한다.
-        - erase 이후에 write 되는 erase 영역을 mark 영역으로 정의하고 마킹한다.
-        - mark 영역에 erase를 수행할지 말지를 0/1로 구분하여 경우의 수를 생성한다.
-        - 경우의 수는 erase 1번 write 4번인 경우 2^4 = 16개가 생성된다.
-        - 16개의 erase 경우에 수에 대해, 최소로 erase 하는 카운트 + write 카운트가 최적해가 된다. (라고 믿고 싶다)
-        - erase를 최소로 하는 카운트는 greedy로 순차 선택하여도 최적해가 나온다. (고 밑고 있다)
-        - 순회 하는 중,  current_count와 동률이 나올 경우 skip 후 다음 해를 시도 한다.
-        """
-        # project values
-        erase_lst = self.project_erase(buffer_lst)
-        write_lst = self.project_write(buffer_lst)
-        check_lst = [i for i, val in enumerate(erase_lst) if val == VALUE_MARK]
+        print()
+        # trivial case
+        if len(buffer_lst) == 0 or len(buffer_lst) == 1:
+            return buffer_lst
 
-        # iteration
+        # '0'으로 write하는 명령은 erase 명령으로 교체한다.
+        replaced_buffer_lst = self.replace_zero_write(buffer_lst)
 
-        # find best
+        # erase 영역을 모은다.
+        erase_mask = self.project_erase(replaced_buffer_lst)
+        self.print_format("ERASE_MASK", erase_mask)
 
-        # compare
+        # write 영역을 모은다.
+        write_mask, write_value = self.project_write(replaced_buffer_lst)
+        self.print_format("WRITE_MASK", write_mask)
 
-        # return
-        return buffer_lst
+        # erase, write mask를 합친다.
+        merged_mask = self.merge_erase_and_write(erase_mask, write_mask)
+        self.print_format("MERGED_MASK", merged_mask)
 
+        # write에 의해 overwrite 되는 erase 영역을 찾는다.
+        overwritten_idx = [i for i, x in enumerate(write_mask) if x == VALUE_VALID]
+        self.print_format("OVER_WRITTEN_IDX", overwritten_idx, lst_seperator=',')
+
+        best_erase_mask = erase_mask
+        best_erase_cmd_lst = [cmd for cmd in replaced_buffer_lst if cmd.COMMAND == "E"]
+        best_write_cmd_lst = [Packet(COMMAND="W", ADDR=i, VALUE=pkt[1])
+                              for i, pkt in enumerate(zip(write_mask, write_value))
+                              if pkt[0] == VALUE_VALID]
+
+        # overwrite 영역은 erase 해도 되고 안해도 된다.
+        # overwrite 영역을 하나씩 선택 하면서, 최소 erase 횟수를 찾는다.
+        try_cnt = 1
+        for r in range(0, len(overwritten_idx) + 1):
+            for selected_element in combinations(overwritten_idx, r):
+                next_erase_mask = merged_mask.copy()
+                for idx in selected_element:
+                    next_erase_mask[idx] = VALUE_EMPTY
+
+                current_erase_cmd_cnt = 0
+                counting = False
+                counting_cnt = 0
+                current_erase_cmd_lst = []
+                erase_cmd_new = None
+                for i, value in enumerate(next_erase_mask):
+                    if value == VALUE_VALID:
+                        if not counting:
+                            counting = True
+                            counting_cnt = 1
+                            erase_cmd_new = Packet(COMMAND="E", ADDR=i)
+                        else:
+                            if counting_cnt == 9:
+                                current_erase_cmd_cnt += 1
+                                erase_cmd_new.SIZE = 10
+                                current_erase_cmd_lst.append(erase_cmd_new)
+                                erase_cmd_new = None
+                                counting = False
+                                counting_cnt = 0
+                            else:
+                                counting_cnt += 1
+                    elif value == VALUE_EMPTY:
+                        if counting:
+                            current_erase_cmd_cnt += 1
+                            erase_cmd_new.SIZE = counting_cnt
+                            current_erase_cmd_lst.append(erase_cmd_new)
+                            erase_cmd_new = None
+                            counting = False
+                            counting_cnt = 0
+
+                if current_erase_cmd_cnt < len(best_erase_cmd_lst):
+                    best_erase_mask = next_erase_mask
+                    best_erase_cmd_lst = current_erase_cmd_lst
+
+                try_cnt += 1
+                self.print_format(f"#{try_cnt:>3},r={r},selected={selected_element},"
+                                  f"result={current_erase_cmd_cnt},best={len(best_erase_cmd_lst)}",
+                                  next_erase_mask)
+
+                # 최적값을 찾은 경우 탐색 중단
+                #if len(best_erase_cmd_lst) <= 1:
+                #    break
+
+        self.print_format("ERASE_MASK", erase_mask)
+        self.print_format("FINAL_ERASE_BEST", best_erase_mask)
+        self.print_format("WRITE_MASK", write_mask)
+        self.print_format("MERGED_MASK", merged_mask)
+
+        # validate
+        # 2. 기존과 길이가 같은 경우, 기존 명령셋을 그대로 리턴한다.
+        # 3. e 명령어 lba 범위, 길이 체크, w 명령어 lba 범위 체크, 값 0 아닌지 체크
+
+        print(f"INPUT CMD(ORG),{len(buffer_lst)}={buffer_lst}")
+        final_cmd_lst = best_erase_cmd_lst + best_write_cmd_lst
+        if len(final_cmd_lst) < len(buffer_lst):
+            print(f"FINAL CMD(OPT),{len(final_cmd_lst)}={final_cmd_lst}")
+            return final_cmd_lst
+        else:
+            print(f"FINAL CMD(ORG),{len(buffer_lst)}={buffer_lst}")
+            return buffer_lst
+
+    def replace_zero_write(self, buffer_lst: list[Packet]) -> list[Packet]:
+        new_lst = []
+        for item in buffer_lst:
+            if item.COMMAND == "W" and item.VALUE == 0:
+                new_lst.append(Packet(COMMAND="E", ADDR=item.ADDR, SIZE=1))
+            else:
+                new_lst.append(item)
+        return new_lst
 
     def project_erase(self, buffer_lst: list[Packet]) -> list[int]:
-        result = [0] * 100 # LBA 0~99
+        mask = [0] * 100  # LBA 0~99
         for cmd in buffer_lst:
             if cmd.COMMAND == "E":
                 for i in range(cmd.SIZE):
-                    result[cmd.ADDR + i] = VALUE_VALID
-        return result
+                    mask[cmd.ADDR + i] = VALUE_VALID
+        return mask
 
-    def project_write(self, buffer_lst) -> list[int]:
-        result = [0] * 100  # LBA 0~99
+    def project_write(self, buffer_lst) -> (list[int], list[int]):
+        mask = [0] * 100  # LBA 0~99
+        value = [0] * 100
         for cmd in buffer_lst:
             if cmd.COMMAND == "E":
                 for i in range(cmd.SIZE):
-                    result[cmd.ADDR + i] = VALUE_EMPTY # overwrite
+                    mask[cmd.ADDR + i] = VALUE_EMPTY  # overwrite
             elif cmd.COMMAND == "W":
-                result[cmd.ADDR] = VALUE_VALID # written
-        return result
+                mask[cmd.ADDR] = VALUE_VALID  # written
+                value[cmd.ADDR] = cmd.VALUE
+        return mask, value
+
+    def merge_erase_and_write(self, erase_lst, write_lst) -> list[int]:
+        mask = erase_lst.copy()
+        for i in range(len(write_lst)):
+            if write_lst[i] == VALUE_VALID:
+                mask[i] = VALUE_VALID
+
+        return mask
+
+    def print_format(self, prefix, lst, lst_seperator=''):
+        print(f"{prefix:>50} [{lst_seperator.join(map(str, lst))}]")
