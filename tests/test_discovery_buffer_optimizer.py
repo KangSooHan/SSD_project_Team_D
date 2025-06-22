9be4d37a0f3ce76da15bc686625685fcf336b763
jiyoung61 <resiyo@naver.com>
2025-06-22 22:31:58 +0900

    [feat] optimizer 테스트 케이스 추가

--- tests/test_discovery_buffer_optimizer.py
+++ tests/test_discovery_buffer_optimizer.py
@@ -332,3 +332,107 @@ def test_Write영역에_해당하는_주소를_1로_마킹해서_리턴한다_7(
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]
+
+
+def test_W_명령값이_0_인경우_E명령으로_대체한다_1():
+    algo = DiscoveryBufferOptimizer()
+
+    tc = [
+        Packet(COMMAND="W", ADDR=3, VALUE=0),
+    ]
+
+    result = algo.replace_zero_write(tc)
+
+    assert len(result) == 1
+    assert result[0].COMMAND == "E"
+    assert result[0].ADDR == 3
+    assert result[0].SIZE == 1
+
+
+def test_W_명령값이_0_인경우_E명령으로_대체한다_2():
+    algo = DiscoveryBufferOptimizer()
+
+    tc = [
+        Packet(COMMAND="W", ADDR=1, VALUE=1),
+        Packet(COMMAND="W", ADDR=3, VALUE=0),
+        Packet(COMMAND="W", ADDR=2, VALUE=2),
+    ]
+
+    result = algo.replace_zero_write(tc)
+
+    assert len(result) == 3
+    assert result[0] == tc[0]
+    assert result[2] == tc[2]
+
+    assert result[1].COMMAND == "E"
+    assert result[1].ADDR == 3
+    assert result[1].SIZE == 1
+
+@pytest.mark.parametrize("tc", [
+    [],
+    [Packet(COMMAND="W", ADDR=0, VALUE=1)],
+    [Packet(COMMAND="E", ADDR=0, VALUE=1)],
+])
+def test_명령어가_1개이하인경우_최적화하지_않는다(optimizer, tc):
+    result = optimizer.calculate(tc)
+
+    assert result == tc
+
+
+@pytest.mark.parametrize("tc, expected_cmd", [
+    #1. 중복 erase 제거
+    [
+        [Packet(COMMAND="E", ADDR=0, SIZE=5), Packet(COMMAND="E", ADDR=0, SIZE=5)],
+        [Packet(COMMAND="E", ADDR=0, SIZE=5)]
+    ],
+    [
+        [Packet(COMMAND="E", ADDR=10, SIZE=5), Packet(COMMAND="E", ADDR=10, SIZE=2)],
+        [Packet(COMMAND="E", ADDR=10, SIZE=5)]
+    ],
+
+    # 2. 중첩, 연속 erase merge (총 길이 10 이하)
+    [
+        [Packet(COMMAND="E", ADDR=0, SIZE=5), Packet(COMMAND="E", ADDR=5, SIZE=5)],
+        [Packet(COMMAND="E", ADDR=0, SIZE=10)]
+    ],
+    [
+        [Packet(COMMAND="E", ADDR=10, SIZE=5), Packet(COMMAND="E", ADDR=10, SIZE=7)],
+        [Packet(COMMAND="E", ADDR=10, SIZE=7)]
+    ],
+    [
+        [Packet(COMMAND="E", ADDR=10, SIZE=5), Packet(COMMAND="E", ADDR=10, SIZE=7), Packet(COMMAND="E", ADDR=12, SIZE=3)],
+        [Packet(COMMAND="E", ADDR=10, SIZE=7)]
+    ],
+
+    # 3. 중첩, 연속 erase merge (총 길이 10 이상)
+    [
+        [Packet(COMMAND="E", ADDR=0, SIZE=7), Packet(COMMAND="E", ADDR=7, SIZE=7), Packet(COMMAND="E", ADDR=14, SIZE=6)],
+        [Packet(COMMAND="E", ADDR=0, SIZE=10), Packet(COMMAND="E", ADDR=10, SIZE=10)]
+    ],
+
+    # 4. erase에 의해 overwrite 되는 write 명령 제거
+    [
+        [Packet(COMMAND="W", ADDR=1, VALUE=0x1), Packet(COMMAND="W", ADDR=3, VALUE=0x3), Packet(COMMAND="W", ADDR=5, VALUE=0x5),
+         Packet(COMMAND="E", ADDR=0, SIZE=10)],
+        [Packet(COMMAND="E", ADDR=0, SIZE=10)]
+    ],
+
+    # 5. erase가 write 보다 먼저 수행되는 경우 wirte 명령 유지
+    [
+        [Packet(COMMAND="E", ADDR=0, SIZE=10),
+        Packet(COMMAND="W", ADDR=1, VALUE=0x1), Packet(COMMAND="W", ADDR=3, VALUE=0x3), Packet(COMMAND="W", ADDR=5, VALUE=0x5)],
+        [Packet(COMMAND="E", ADDR=0, SIZE=10),
+         Packet(COMMAND="W", ADDR=1, VALUE=0x1), Packet(COMMAND="W", ADDR=3, VALUE=0x3), Packet(COMMAND="W", ADDR=5, VALUE=0x5)]
+    ],
+
+    # 6. write로 덮어지는 영역이 2개 이상의 erase 영역을 접합하는 경우
+    [
+        [Packet(COMMAND="E", ADDR=0, SIZE=2), Packet(COMMAND="E", ADDR=3, SIZE=2), Packet(COMMAND="E", ADDR=6, SIZE=2),
+        Packet(COMMAND="W", ADDR=2, VALUE=0x2), Packet(COMMAND="W", ADDR=5, VALUE=0x5)],
+        [Packet(COMMAND="E", ADDR=0, SIZE=8),
+         Packet(COMMAND="W", ADDR=2, VALUE=0x2), Packet(COMMAND="W", ADDR=5, VALUE=0x5)]
+    ],
+])
+def test_buffer_최적화_반환_커맨드_동일_케이스_검증(optimizer, tc, expected_cmd):
+    result = optimizer.calculate(tc)
+    assert result == expected_cmd
