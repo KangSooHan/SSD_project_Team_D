faa60d783a948695c2c0b3a61a4bbcdab9911774
jiyoung61 <resiyo@naver.com>
2025-06-23 16:16:12 +0900

    [style] to_4byte_hex_str() 메서드 테스트 코드 포맷팅

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,7 @@ from utils import to_4byte_hex_str
 @pytest.mark.parametrize("input_int, expected_str", [
     (0, "0x00000000"),
     (1, "0x00000001"),
-    (2**31 - 1, "0x7FFFFFFF")
+    (2 ** 31 - 1, "0x7FFFFFFF")
 ])
 def test_입력된정수를_16진수_문자열로_변환한다(input_int: int, expected_str: str):
     converted_str = to_4byte_hex_str(input_int)
